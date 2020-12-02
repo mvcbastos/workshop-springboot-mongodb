cbc4075252c78729fa686462381ec85bd71735b6
Marcus Bastos <mvcbastos@gmail.com>
2020-12-02 13:07:01 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/marcusbastos/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.marcusbastos.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcusbastos.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
