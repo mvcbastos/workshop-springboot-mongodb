4e36d7495c2311c5ab9e5a0e978af5385c89bf50
Marcus Bastos <mvcbastos@gmail.com>
2020-12-03 08:47:36 -0300

    Consulta simples com @Query

--- src/main/java/com/marcusbastos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcusbastos/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.marcusbastos.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marcusbastos.workshopmongo.domain.Post;
@@ -10,5 +11,9 @@ import com.marcusbastos.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	// Quando for necessário aproveitar o parâmetro do método na consulta, usa-se ?, o 0 é pra indicar que é o primeiro parâmetro
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
