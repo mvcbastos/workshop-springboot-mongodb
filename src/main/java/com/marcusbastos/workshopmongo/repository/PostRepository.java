810a518d1f4749fa5214bcddef5f2e42e83eed04
Marcus Bastos <mvcbastos@gmail.com>
2020-12-03 09:51:52 -0300

    Consulta com vários critérios

--- src/main/java/com/marcusbastos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcusbastos/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.marcusbastos.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -16,4 +17,8 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	// Como comments é uma collection, eu tenho que especificar o campo da collection cujo conteúdo eu quero comparar, colocando um ponto após o campo 'comments'	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
